70c9af767d082f2a6e6d8ab69dd1e64bbd0ea1f4
vinit4a <vinit.k.addweb@gmail.com>
2021-11-01 10:10:21 +0530

    calculator

---
+++ calculator/index.js
@@ -0,0 +1,24 @@
+
+        let screen= document.getElementById("screen");
+        buttons= document.querySelectorAll("button");
+        let screenValue =' ';
+        for(item of buttons){
+            item.addEventListener('click',(e)=>{
+               buttonText=e.target.innerText;
+               console.log("button text is ", buttonText);
+               if(buttonText=='='){
+                   screen.value=eval(screenValue); 
+               }
+               else if (buttonText=='C') {
+                   screenValue = "";
+                   screen.value= screenValue;
+
+               }
+               else{
+                   screenValue+= buttonText;
+                   screen.value = screenValue;
+               }
+            })
+           
+        }
+    
