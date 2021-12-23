2ca5a94827c6b13cf447262c511d87991741a9cb
vinit <vinit.k.addweb@gmail.com>
2021-12-24 00:02:16 +0530

    vue project

--- vue1/learning resource app/prj-cmp-01-starting-setup/src/App.vue
+++ vue1/learning resource app/prj-cmp-01-starting-setup/src/App.vue
@@ -1,41 +1,33 @@
 <template>
-  <ul>
-    <li>
-      <learning-resource
-        v-for="res in StoredResources"
-        :key="res.id"
-        :title="res.title"
-        :description="res.description"
-        :link="res.link"
-      >
-      </learning-resource>
-    </li>
-  </ul>
+<the-header title="RememberMe"></the-header>
+<the-resource></the-resource>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+
+import TheResource from './components/learning-resources/TheResource.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 export default {
   components: {
-    LearningResource: LearningResource,
+    
+    TheResource,TheHeader
   },
-  data() {
-    return {
-      StoredResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The offical Vue.js documentation.',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'Google',
-          title: 'Official Guide',
-          description: 'Learn to Google.',
-          link: 'https://google.org',
-        },
-      ],
-    };
-  },
+ 
 };
 </script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+  
+}
+</style>
---
+++ vue1/learning resource app/prj-cmp-01-starting-setup/src/components/learning-resources/TheResource.vue
@@ -0,0 +1,59 @@
+<template>
+    <base-card>
+    <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode">
+    Stored Resource
+    </base-button>
+    <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">
+    Add Resource
+    </base-button>
+    </base-card>
+    <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResource.vue';
+import AddResource from './AddResource.vue';
+export default {
+    components:{
+        StoredResources,AddResource
+    },
+    data(){
+        return{
+           selectedTab:'stored-resources',
+           storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The offical Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'Google',
+          title: 'Google Guide',
+          description: 'Learn to Google.',
+          link: 'https://google.org',
+        },
+      ],
+        };
+    },
+    provide(){
+        return{
+            resources:this.storedResources 
+        }
+    },
+    computed:{
+        storedResButtonMode(){
+            return this.selectedTab === 'stored-resources' ? null : 'flat';
+        },
+        addResButtonMode(){
+            return this.selectedTab === 'add-resource' ? null : 'flat';
+        }
+    },
+    methods:{
+        setSelectedTab(tab){
+            this.selectedTab = tab;
+        }
+    }
+}
+
+</script>
